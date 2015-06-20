aa844f19fb5600c39e09f324b4b22bf51297fd36
Marcio Marchini <mqm@sglebs.com>
2015-06-20 15:32:56 -0300

    more tests

--- scratch/atomac-test.py
+++ scratch/atomac-test.py
@@ -1,20 +1,39 @@
 import atomac
-atomac.launchAppByBundleId('com.apple.Automator')
-automator = atomac.getAppRefByBundleId('com.apple.Automator')
-print automator.windows()
-window = automator.windows()[0]
+import time
+atomac.launchAppByBundleId('com.apple.calculator')
+calc = atomac.getAppRefByBundleId('com.apple.calculator')
+time.sleep(2)
+window = calc.windows()[0]
+print window
 print window.AXTitle
 print window.getAttributes()
+print window.getActions()
 print window.AXIdentifier
 print window.AXChildren
-sheet = window.sheets()[0]
-print sheet
-print sheet.AXIdentifier
-print sheet.buttons()
-close = sheet.buttons('Close')[0]
-print close
-print close.AXIdentifier
-print close.getActions()
-print close.getAttributes()
-print close.AXTitle
-close.Press()
+print window.buttons()
+window.sendKeys ("234*2")
+
+
+
+# atomac.launchAppByBundleId('com.apple.Automator')
+# automator = atomac.getAppRefByBundleId('com.apple.Automator')
+# print automator.windows()
+# window = automator.windows()[0]
+# print window.AXTitle
+# print window.getAttributes()
+# print window.AXIdentifier
+# print window.sheets()
+# #print window.AXChildren
+# sheet = window.sheets()[0]
+# print sheet
+# print sheet.AXIdentifier
+# #print sheet.AXTitle
+# print sheet.AXChildren
+# print sheet.buttons()
+# close = sheet.buttons('Close')[0]
+# print close
+# print close.AXIdentifier
+# print close.getActions()
+# print close.getAttributes()
+# print close.AXTitle
+# close.Press()
